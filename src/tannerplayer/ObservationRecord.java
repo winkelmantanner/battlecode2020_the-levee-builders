db61a8c419d58c3354497484d00dca7057cf4ecf
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2020-01-08 09:49:42 -0600

    More stuff

--- src/tannerplayer/ObservationRecord.java
+++ src/tannerplayer/ObservationRecord.java
@@ -8,6 +8,7 @@ public strictfp class ObservationRecord {
     public int turnOfObservation;
     public boolean was_flooded;
     public RobotInfo building_if_any;
+    public int crude_soup;
 
     public ObservationRecord(final RobotController rc, final MapLocation location) throws GameActionException {
       // throws if not rc.canSenseLocation(location) 
@@ -15,6 +16,7 @@ public strictfp class ObservationRecord {
       turnOfObservation = rc.getRoundNum();
       was_flooded = rc.senseFlooding(location);
       building_if_any = rc.senseRobotAtLocation(location);
+      crude_soup = rc.senseSoup(location);
       if(building_if_any != null
         && !(
              building_if_any.getType() == RobotType.HQ
