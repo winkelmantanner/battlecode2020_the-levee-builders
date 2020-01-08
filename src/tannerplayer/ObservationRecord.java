d7b7d013f941625ab832a38a194a2452394f7e45
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2020-01-08 14:15:00 -0600

    Make the miner not get stuck when the path home is obstructed

--- src/tannerplayer/ObservationRecord.java
+++ src/tannerplayer/ObservationRecord.java
@@ -9,14 +9,16 @@ public strictfp class ObservationRecord {
     public boolean was_flooded;
     public RobotInfo building_if_any;
     public int crude_soup;
+    public int where_ive_been_index; // must be set manually
 
-    public ObservationRecord(final RobotController rc, final MapLocation location) throws GameActionException {
+    public ObservationRecord(final RobotController rc, final MapLocation location, final int where_ive_been_index_val) throws GameActionException {
       // throws if not rc.canSenseLocation(location) 
       elevation = rc.senseElevation(location);
       turnOfObservation = rc.getRoundNum();
       was_flooded = rc.senseFlooding(location);
       building_if_any = rc.senseRobotAtLocation(location);
       crude_soup = rc.senseSoup(location);
+      where_ive_been_index = where_ive_been_index_val;
       if(building_if_any != null) {
         switch(building_if_any.getType()) {
           case MINER:
