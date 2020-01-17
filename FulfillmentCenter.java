11faf31837d08e0ffaef032129ae9878e92938a3
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2020-01-16 18:49:34 -0600

    Make fulfillment centers not build drones if near a net gun.  This is not well tested

--- FulfillmentCenter.java
+++ FulfillmentCenter.java
@@ -17,10 +17,22 @@ public strictfp class FulfillmentCenter extends Building {
     }
     public void runTurn() throws GameActionException {
         if(num_drones_built < 3) {
-            for (Direction dir : directions) {
-                boolean did_build = tryBuild(RobotType.DELIVERY_DRONE, dir);
-                if(did_build)
-                    num_drones_built++;
+            RobotInfo [] nearby_enemy_robots = rc.senseNearbyRobots(
+                rc.getType().sensorRadiusSquared,
+                rc.getTeam().opponent()
+            );
+            boolean is_safe_to_build_drones = true;
+            for(RobotInfo rbt : nearby_enemy_robots) {
+                if(rbt.type == RobotType.NET_GUN) {
+                    is_safe_to_build_drones = false;
+                }
+            }
+            if(is_safe_to_build_drones) {
+                for (Direction dir : directions) {
+                    boolean did_build = tryBuild(RobotType.DELIVERY_DRONE, dir);
+                    if(did_build)
+                        num_drones_built++;
+                }
             }
         }
     }
