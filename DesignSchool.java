847bd4addf9b743b4fd33cabf06a0af6d578cf99
Tanner Winkelman <winkelmantanner@gmail.com>
2020-01-25 15:55:02 -0600

    Base design school behavior on whether or not hq sends distress signal

--- DesignSchool.java
+++ DesignSchool.java
@@ -15,14 +15,45 @@ public class DesignSchool extends Building {
         super(rc);
         this.rc = rc;
     }
+
+    int last_round_checked_for_distress_signal = 1;
+    boolean has_seen_distress_signal = false;
+
     public void runTurn() throws GameActionException {
         updateLocOfHQ();
         Direction dir_to_build = null;
         int dir_to_build_dist_from_hq = 12345;
+        
+        int elev_of_highest_buildable_adj_loc = -12345;
+        for(Direction dir : directions) {
+            MapLocation l = rc.adjacentLocation(dir);
+            if(rc.canSenseLocation(l)
+                && rc.canSenseLocation(rc.getLocation())
+                && rc.senseElevation(l) > elev_of_highest_buildable_adj_loc
+                && rc.senseElevation(l) >= rc.senseElevation(rc.getLocation()) - MAX_ELEVATION_STEP
+                && rc.senseElevation(l) <= rc.senseElevation(rc.getLocation()) + MAX_ELEVATION_STEP
+            ) {
+                elev_of_highest_buildable_adj_loc = rc.senseElevation(l);
+            }
+        }
+        boolean should_build_a_lot_of_landscapers = has_seen_distress_signal
+            || (rc.canSenseLocation(rc.getLocation())
+                && (
+                    GameConstants.getWaterLevel(rc.getRoundNum() + 100) >= rc.senseElevation(rc.getLocation())
+                    || GameConstants.getWaterLevel(rc.getRoundNum() + 100) >= elev_of_highest_buildable_adj_loc
+                )
+            );
+
         for (Direction dir : directions) {
             if(locOfHQ != null) {
                 if(rc.canBuildRobot(RobotType.LANDSCAPER, dir)
-                    && rc.getTeamSoup() > RobotType.LANDSCAPER.cost * (1.2 + (((double)num_landscapers_built) / 3))
+                    && rc.getTeamSoup() > RobotType.LANDSCAPER.cost * (1.2 + (((double)num_landscapers_built)
+                        / (should_build_a_lot_of_landscapers
+                                ? 3
+                                : 0.5
+                            )
+                        )
+                    )
                     && num_landscapers_built < 10
                 ) {
                     MapLocation ml = rc.getLocation().add(dir);
@@ -57,5 +88,17 @@ public class DesignSchool extends Building {
             rc.buildRobot(RobotType.LANDSCAPER, dir_to_build);
             num_landscapers_built++;
         }
+        while(last_round_checked_for_distress_signal + 1 < rc.getRoundNum()
+            && Clock.getBytecodesLeft() > 1000
+        ) {
+            last_round_checked_for_distress_signal++;
+            for(int [] my_message : getMyMessages(last_round_checked_for_distress_signal)) {
+                if(my_message[0] == MessageType.DISTRESS_SIGNAL.getValue()) {
+                    has_seen_distress_signal = true;
+                    System.out.println("Received distress signal from HQ");
+                }
+            }
+        }
+
     }
 }
